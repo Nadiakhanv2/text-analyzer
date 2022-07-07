f680cc401173f2c82a616d3791116cfe3cc30565
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-07 22:08:47 +0500

    Add Showalert to text-analyzer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    <TextForm heading="Text Analyzer"/>
-  </div>;
+
+  const [alert, setAlert] = useState("null");
+  
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => { 
+      setAlert(null);
+    }, 1500);
+  }
+  return (
+    <div className="App">
+      <Alert alert={alert} />
+
+      <TextForm heading="Text Analyzer" showAlert={showAlert} />
+    </div>
+  );
 }
 
 export default App;
